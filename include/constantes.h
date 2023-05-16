624073593a2b434b64cff26b08a93fe41430c38b
llchika <llchika@proton.me>
2023-05-16 18:48:38 +0200

    modified:   include/constantes.h 	modified:   ressources/maps/map_lvl1.csv

--- include/constantes.h
+++ include/constantes.h
@@ -2,14 +2,14 @@
 #define _constantes_H
 
 namespace JeuESIR {
-	//! Constantes d�finissant la taille de la tile.
+	//! Constantes définissant la taille de la tile.
 	static const int originalTileSize=32;			//16 x 16 Tiles
-	static const int tileSize=originalTileSize * 1;	//1 -> scale   // 32 x 1 = 32 Tile (32 x 32)
+	static const int tileSize=originalTileSize*1;	//1 -> scale   // 32 x 1 = 32 Tile (32 x 32)
 
-	//! Constantes d�finissant la taille du screen en fonction des tiles.
-	static const int maxScreenCol=16;
-	static const int maxScreenRow=20;
-	static const int screenWidth=tileSize * maxScreenCol;	//480
-	static const int screenHeight=tileSize * maxScreenRow;//608
+	//! Constantes définissant la taille du screen en fonction des tiles.
+	static const int maxScreenCol=33;
+	static const int maxScreenRow=21;
+	static const int screenWidth=tileSize*maxScreenCol;	  //480
+	static const int screenHeight=tileSize*maxScreenRow;  //608
 }
 #endif
