1815edb456e1481f04d5a3174a32e477e57d8369
Baptiste <amice.bap@gmail.com>
2023-05-17 00:27:42 +0200

    bonne  taille de fenetre

--- include/constantes.h
+++ include/constantes.h
@@ -6,14 +6,6 @@ namespace JeuESIR {
 	static const int originalTileSize = 32;			//16 x 16 Tiles
 	static const int tileSize = originalTileSize * 1;	//1 -> scale   // 32 x 1 = 32 Tile (32 x 32)
 
-	//! Constantes d�finissant la taille du screen en fonction des tiles.
-	static const int maxScreenCol = 16;
-	static const int maxScreenRow = 20;
-	static const int screenWidth = tileSize * maxScreenCol;	//480
-	static const int screenHeight = tileSize * maxScreenRow;//608
-	static const int heroSize = 16;	
-
-/*
 	//! Constantes définissant la taille du screen en fonction des tiles.
 	static const int maxScreenCol=33;
 	static const int maxScreenRow=21;
--- src/entity/Mouse.cpp
+++ src/entity/Mouse.cpp
@@ -1,6 +1,6 @@
 #include <Mouse.h>
-
 #include <constantes.h>
+#include <Collision.h>
 Mouse::Mouse(Map* map, const char* textureSheet, const Vector2<int>& position, std::string name)
 : Entity(map, textureSheet, position, name) {
     initPersonnge();
@@ -10,5 +10,10 @@ Mouse::~Mouse() {}
 
 void Mouse::update(Vector2<int> mousePos){
     setPosition(mousePos);
+
+    
+
+
+
     GameObject::update();
 }
