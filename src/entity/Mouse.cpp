b854f32e6039b15ab7a2aad83184e1a65d4afee5
ulysse-neo <u-neo.lartigaud@etu.uca.fr>
2023-05-16 14:55:49 -0400

    Mise à jour de Entity

--- src/entity/Mouse.cpp
+++ src/entity/Mouse.cpp
@@ -1,9 +1,9 @@
 #include <Mouse.h>
 
-
+#include <constantes.h>
 
 Mouse::Mouse(Map* map, const char* textureSheet, const Vector2<int>& position, std::string name)
-: Entity(map, textureSheet, position, name) {
+: Entity(map, textureSheet, position, name, Vector2<int>(JeuESIR::heroSize, JeuESIR::heroSize)) {
     initPersonnge();
 }
 
