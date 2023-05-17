90656fb8c1daa3272a8bba41f5a629481b3e5ea5
Baptiste A <amice.bap@gmail.com>
2023-05-17 08:50:32 +0200

    ennemis bouge vite fait

--- src/entity/Ennemy.cpp
+++ src/entity/Ennemy.cpp
@@ -9,16 +9,35 @@ Ennemy::Ennemy(Map* map, const char* textureSheet, const Vector2<int>& position,
 Ennemy::~Ennemy() {}
 
 void Ennemy::update(Vector2<int> const& target) {
+	
+
+
 	//prit m_timer
-	std::cout << "Ennemy::update()" << std::endl;
-	//move the target in a random direction
-	//int x = rand() % 3 - 1;
-	//int y = rand() % 3 - 1;
-	//setPosition(getPosition() + Vector2<int>(x, y));
+	std::cout << m_timer << std::endl;
+
+	//translate of on ex or y toward the target
+	if (m_timer > 50) {
+		if (getPosition()[0] < target[0]) {
+			setPosition(Vector2<int>(getPosition()[0] + m_speed, getPosition()[1]));
+		}
+		else if (getPosition()[0] > target[0]) {
+			setPosition(Vector2<int>(getPosition()[0] - m_speed, getPosition()[1]));
+		}
+		if (getPosition()[1] < target[1]) {
+			setPosition(Vector2<int>(getPosition()[0], getPosition()[1] + m_speed));
+		}
+		else if (getPosition()[1] > target[1]) {
+			setPosition(Vector2<int>(getPosition()[0], getPosition()[1] - m_speed));
+		}
+
+		m_timer = 0;
+	}
+
 
 	if (m_health <= 0) {
 		//delete this; fait planter (probleme de mémoire)
 		//setStatus(Status::DESTROY); //Status est privé dans ce contexte donc ca ne compile pas
 	}
 	GameObject::update();
+	m_timer += 1;
 }
